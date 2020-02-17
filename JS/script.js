e6788dd73f710dd4165a45b72a1be79a7e7a9f2d
miguelcarmona23 <miguelcarmona996@gmail.com>
2020-02-17 16:27:43 -0500

    Added cosnt labels

--- JS/script.js
+++ JS/script.js
@@ -1,3 +1,14 @@
 document.querySelector("#show-password").addEventListener("input", function() {
     document.querySelector("#password").type = this.checked ? "text" : "password";
 });
+
+const labels = document.querySelectorAll("#login-form label input");
+for (let x = 0; x < labels.length; x++) {
+    labels[x].addEventListener("focus", function() {
+        this.parentNode.classList.add("state-focus");
+    });
+
+    labels[x].addEventListener("blur", function() {
+        this.parentNode.classList.remove("state-focus");
+    });
+}
