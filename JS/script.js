dd01a5813a7e6bd767bef146608c645705a62c45
miguelcarmona23 <miguelcarmona996@gmail.com>
2020-02-17 16:30:10 -0500

    Added form validation submission

--- JS/script.js
+++ JS/script.js
@@ -12,3 +12,29 @@ for (let x = 0; x < labels.length; x++) {
         this.parentNode.classList.remove("state-focus");
     });
 }
+
+document.querySelector(".login-form").addEventListener("submit", function(e) {
+    let errorMessage = "";
+    const error = document.querySelector("#error-message");
+    const username = document.querySelector("#username");
+    const password = document.querySelector("#password");
+
+    error.textContent = "";
+
+    // check password length
+    if (password.value.length < 8) {
+        errorMessage = "Password is mandatory";
+        password.parentNode.classList.add("state-error");
+        password.focus();
+        // check email length
+    } else if (username.value.length < 5) {
+        errorMessage = "Username is too short (min 5 chars)";
+        username.parentNode.classList.add("state-error");
+        username.focus();
+    }
+
+    if (errorMessage != "") {
+        error.textContent = errorMessage;
+        e.preventDefault();
+    }
+});
